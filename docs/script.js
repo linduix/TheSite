0ad23002b1961272aa97ffeb52b8e92b47dfd89c
linduix <abdullahhkhan3@gmail.com>
2022-09-24 16:10:35 +0500

    Updating Scripts

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,24 @@
-script.js
+var name = localStorage.getItem("name")
+if (name == "null" || !name) {
+	name = prompt("What's your name?")
+	localStorage.setItem("name", name)
+	console.log(name)
+} else {
+	document.getElementById("title").innerHTML = "Welcome Back "+name;
+	console.log(typeof name)
+}
+
+function show() {
+	document.body.style.filter = "blur(0px)";
+}
+
+function hide() {
+	document.body.style.filter = "blur(20px)"
+	setTimeout(show, 2000);
+	console.log('done');
+}
+
+function move() {
+	let cover = document.querySelector(".n1 .left .cover")
+	cover.style.transform = 'translateY(100%)'
+}
